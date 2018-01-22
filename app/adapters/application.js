5239a6ed58eaf4a7906f24e87ede7266eaca3ca3
Jay Varner <jayvarner@gmail.com>
2018-01-22 15:55:06 -0500

    Fixes for linter.

--- app/adapters/application.js
+++ app/adapters/application.js
@@ -1,5 +1,4 @@
 import { inject as service } from '@ember/service';
-import { computed } from '@ember/object';
 import DS from 'ember-data';
 import ENV from '../config/environment';
 
@@ -9,11 +8,6 @@ export default JSONAPIAdapter.extend({
   tenant: service(),
   fastboot: service(),
   host: function host() {
-    console.log(`http://${this.get('tenant.domain')}.${ENV.APP.API_HOST}`)
     return `https://${this.get('tenant.domain')}.${ENV.APP.API_HOST}`;
-    // return 'http://jay.otb.org:3000'
   }.property()
-  // host: computed('tenent.domain', function() {
-  //   return `https://${this.get('tenant.domain')}.${ENV.APP.API_HOST}`;
-  // })
 });
